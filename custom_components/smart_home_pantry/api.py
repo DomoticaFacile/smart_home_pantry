b624b34645921850dfcc67164e411f33e59ab4a6
Domotica Facile <domoticafacile@herobyte.com>
2026-08-27 19:07:44 +0200

    Update USER_AGENT to version 0.4.0

--- custom_components/smart_home_pantry/api.py
+++ custom_components/smart_home_pantry/api.py
@@ -10,7 +10,7 @@ _LOGGER = logging.getLogger(__name__)
 
 BASE_URL = "https://world.openfoodfacts.org/api/v2/product/{barcode}.json?fields=product_name,brands"
 
-USER_AGENT = "SmartHomePantry-HomeAssistant/0.2.2"
+USER_AGENT = "SmartHomePantry-HomeAssistant/0.4.0"
 
 CACHE_TTL_SECONDS = 60 * 60 * 24 * 30
 
